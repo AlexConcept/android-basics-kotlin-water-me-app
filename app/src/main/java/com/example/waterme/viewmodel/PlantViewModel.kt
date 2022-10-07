35bb60c04d8c1db840ed41a08a6e704bd43bd6c0
Oleksandr <alex505osip@gmail.com>
2022-10-07 13:59:50 +0300

    Create a data instance with plantName; Create a work request; Call the workManager method with the necessary parameters and work request; Init test done, self-work complete;

--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -18,19 +18,35 @@ package com.example.waterme.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
-class PlantViewModel(application: Application): ViewModel() {
+
+class PlantViewModel(application: Application) : ViewModel() {
 
     val plants = DataSource.plants
+    private val workManager = WorkManager.getInstance(application)
 
-    internal fun scheduleReminder(
+        internal fun scheduleReminder(
         duration: Long,
         unit: TimeUnit,
         plantName: String
     ) {
+        val data = Data.Builder()
+            .putString(WaterReminderWorker.nameKey, plantName)
+
+        val request = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration, unit)
+            .setInputData(data.build())
+            .build()
+
+        workManager.enqueueUniqueWork(
+            plantName, ExistingWorkPolicy.REPLACE,
+            request
+        )
+
         // TODO: create a Data instance with the plantName passed to it
 
         // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
@@ -41,7 +57,7 @@ class PlantViewModel(application: Application): ViewModel() {
 }
 
 class PlantViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(PlantViewModel::class.java)) {
             PlantViewModel(application) as T
         } else {
